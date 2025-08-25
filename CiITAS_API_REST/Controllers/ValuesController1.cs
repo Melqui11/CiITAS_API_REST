ae7bb2b36659a7d6bcf8fb87e20802228ab1b5ac
Melquisedec Aviles <melquisedecavilesq@gmail.com>
2025-08-25 00:49:07 -0600

    hola prueba

---
+++ CiITAS_API_REST/Controllers/ValuesController1.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CiITAS_API_REST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController1 : ControllerBase
+    {
+    }
+}
